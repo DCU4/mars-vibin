61cb4e566e9d10bbb0157cfa8e3a1eb9653d4291
Dylan Connor <DylanJConnor4@gmail.com>
2021-02-20 21:15:15 -0500

    cleanup

--- src/js/components/presentational/Header.jsx
+++ src/js/components/presentational/Header.jsx
@@ -4,16 +4,13 @@ import React, { Component } from 'react';
 export const Header = (props) => {
 
     return (
-
             <header>
               <p>Mars Vibing</p>
               <select id="dataChange" onChange={props.handleDataChange}>
                 <option value="images">Mars Images</option>
                 <option value="weather">Mars Weather</option>
-                {/* <option value="">Mars Images</option> */}
               </select>
             </header>
-
     );
 
 }
