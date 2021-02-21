5b4a1af9c7c55839b2a6207dc012620af31c590a
Dylan Connor <DylanJConnor4@gmail.com>
2021-02-20 21:15:05 -0500

    set up onload for animation

--- src/js/components/presentational/MarsImages.jsx
+++ src/js/components/presentational/MarsImages.jsx
@@ -4,30 +4,42 @@ export class MarsImages extends Component {
   constructor(props) {
       super(props);
       this.state = {
-        images: []
+        images: [],
+        rover: 'curiosity'
       }
   }
 
-  getData = async () => {
+  getData = async (rover) => {
 
     const api_key = 'Kcscv2CEbTduu4JMlB5U5JHzNHBTQEGkPfZt0DWj';
-    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=MAST&api_key=${api_key}`;
+    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=888&api_key=${api_key}`;
     const call = await fetch(url);
     const data = await call.json();
     console.log('rover pics:',data);
 
     this.setState({
       images: data.photos
-    })
+    });
 
   }
 
-  
+  handleRoverChange = (e) => {
+    this.setState({
+      rover: e.target.value
+    }, () => {
+      this.getData(this.state.rover);
+    });
+  }
+
+  delayAnimation = (i,e) => {
+    e.target.style.animationDelay = .25*i+'s'; 
+  }
 
 
   componentDidMount () {
+    console.log('mount')
     if(this.props.cache.length == 0){
-      this.getData();
+      this.getData(this.state.rover);
     }
   }
 
@@ -39,17 +51,24 @@ export class MarsImages extends Component {
 
   render() {
     
+
+
     if (this.state.images.length == 0 && this.props.cache == 0) {
       return <div id="spinner">Loading Data...<span></span></div>;
     } 
     
     let images = this.props.cache.length == 0 ? 
-      this.state.images.map((img, i) => <img loading="lazy" key={i} src={img.img_src} /> ) :
-      this.props.cache.map((img, i) => <img loading="lazy" key={i} src={img.img_src} /> );
+      this.state.images.map((img, i) => <img onLoad={(e) => this.delayAnimation(i,e)} loading="lazy" key={i} src={img.img_src} /> ) :
+      this.props.cache.map((img, i) => <img onLoad={(e) => this.delayAnimation(i,e)}  loading="lazy" key={i} src={img.img_src} /> );
     
 
     return (
       <div className="wrapper">
+          <select id="rovers" onChange={this.handleRoverChange}>
+            <option value="curiosity">Curiosity</option>
+            <option value="opportunity">Opportunity</option>
+            <option value="spirit">Spirit</option>
+          </select>
         <div className="image-grid">
           {images}
         </div>
